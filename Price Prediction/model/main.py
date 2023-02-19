d38340655ec06efefb9d0cb3efeb8b6e7c32509c
norestrictionss <girayakman@gmail.com>
2023-02-20 01:09:14 +0300

    Files added to repository

---
+++ Price Prediction/model/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+
+dataset = pd.read_csv('Housing.csv')
+dataset = dataset.sample(frac=1)
+dataset = dataset[['price', 'area', 'bedrooms', 'bathrooms']]
+k_folds = KFold(n_splits=3)
+x = dataset[['area', 'bedrooms', 'bathrooms']]
+y = dataset['price']
+scaler = MinMaxScaler()
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+# Model Building Part with Linear Regression
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+lr.predict(x_test)
+print("Actual Result: ")
+print(y_test)
+print("Predictions: ")
+result = lr.predict(x_test)
+result = pd.DataFrame(data=result)
+print(result)
+
+# L1 Regularization
+lasso = Lasso()
+lasso.fit(x_train, y_train)
+print(pd.DataFrame(data=lasso.predict(x_test)))
+scores = cross_val_score(LinearRegression(), x, y, cv=k_folds)
+print(scores)
+
+# Next will come later on.
